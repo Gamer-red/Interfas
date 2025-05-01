6ff8ae023185f3391d61b1dff73d404e1415d337
Gamer-red <jarolguaherrera@gmail.com>
2025-04-30 20:51:24 -0600

    Mejora del modo celular jeje

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const header = document.querySelectorAll('header')[0];
+
+window.addEventListener('scroll', () =>{
+    header.classList.toggle("sticky", window.scrollY > 60);
+})
+
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('nav-active');
+});
